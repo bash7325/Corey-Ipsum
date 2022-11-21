7f493194c3e2a82ff25517918493104727d281d1
Brandon Ashby <bash7325@gmail.com>
2022-11-21 09:17:55 -0700

    fixed repeating sentence problem

--- JS/index.js
+++ JS/index.js
@@ -25,7 +25,8 @@ numOfSentRange.addEventListener("input", syncSentNumbers);
 //get a noun, a verb, another noun, a preposition and a corey word from coreyText and add push to an array
 function getSentences() {
     let sentences = [];
-    for (let i = 0; i < 6; i++) {
+    let sentenceAmount = getSliderValue();
+    for (let i = 0; i < sentenceAmount; i++) {
         let sentence = [];
         sentence.push(coreyText.nouns[Math.floor(Math.random() * coreyText.nouns.length)]);
         sentence.push(coreyText.corey[Math.floor(Math.random() * coreyText.corey.length)]);
@@ -39,15 +40,20 @@ function getSentences() {
     return sentences;
 }
 
-//create the amount of sentences according to the slider value
+//create the amount of sentences according to the slider value, but don't repeat the same sentence twice
 function createSentences() {
     let sentences = getSentences();
     let sentenceAmount = getSliderValue();
     let ipsumText = '';
     for (let i = 0; i < sentenceAmount; i++) {
         let sentence = sentences[Math.floor(Math.random() * sentences.length)];
+        //if the sentence is already in the ipsumText, don't add it again
+        if (ipsumText.includes(sentence.join(' '))) {
+            sentenceAmount++;
+        } else {
         sentence[0] = sentence[0].charAt(0).toUpperCase() + sentence[0].slice(1);
         ipsumText += sentence.join(' ') + '. ';
+        }
     }
     return ipsumText;
 }
